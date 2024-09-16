dba25cc781a8dfda680da287b305fc092759613d
Ricki Angel <angelricki@outlook.com>
2024-09-16 22:18:34 +0100

    Remove overlarge node-modules folder. Clean up CSS to global index.css

--- SW-HOME/src/views/Home.jsx
+++ SW-HOME/src/views/Home.jsx
@@ -17,7 +17,7 @@ const CardIcon = lazy(() => import("../components/card/CardIcon"));
 const CardLogin = lazy(() => import("../components/card/CardLogin"));
 const CardImage = lazy(() => import("../components/card/CardImage"));
 const CardDealsOfTheDay = lazy(() =>
-  import("../components/card/CardDealsOfTheDay")
+    import("../components/card/CardDealsOfTheDay")
 );
 
 class HomeView extends Component {
@@ -37,112 +37,120 @@ class HomeView extends Component {
     const rows = [...Array(Math.ceil(iconProducts.length / 4))];
     // chunk the products into the array of rows
     const productRows = rows.map((row, idx) =>
-      iconProducts.slice(idx * 4, idx * 4 + 4)
+        iconProducts.slice(idx * 4, idx * 4 + 4)
     );
     // map the rows as div.row
     const carouselContent = productRows.map((row, idx) => (
-      <div className={`carousel-item ${idx === 0 ? "active" : ""}`} key={idx}>
-        <div className="row g-3">
-          {row.map((product, idx) => {
-            const ProductImage = this.components[product.img];
-            return (
-              <div key={idx} className="col-md-3">
-                <CardIcon
-                  title={product.title}
-                  text={product.text}
-                  tips={product.tips}
-                  to={product.to}
-                >
-                  <ProductImage className={product.cssClass} width="80" height="80" />
-                </CardIcon>
-              </div>
-            );
-          })}
+        <div className={ `carousel-item ${idx === 0 ? "active" : ""}`} key={idx}>
+          <div className="row g-3">
+            {row.map((product, idx) => {
+              const ProductImage = this.components[product.img];
+              return (
+                  <div key={idx} className="col-md-3">
+                    <CardIcon
+                        title={product.title}
+                        text={product.text}
+                        tips={product.tips}
+                        to={product.to}
+                    >
+                      <ProductImage className={product.cssClass} width="80" height="80" />
+                    </CardIcon>
+                  </div>
+              );
+            })}
+          </div>
         </div>
-      </div>
     ));
 
     return (
-      <React.Fragment>
-        <Banner className="mb-3" id="carouselHomeBanner" data={data.banner} />
-        <div className="container-fluid bg-light mb-3">
-          <div className="row g-3">
-            <div className="col-md-9">
-              <Carousel id="elect-product-category" className="mb-3">
-                {carouselContent}
-              </Carousel>
-              <Support />
-            </div>
-            <div className="col-md-3">
-              <CardLogin className="mb-3" />
-              <CardImage src="../../images/banner/Watches.webp" to="promo" />
-            </div>
-          </div>
-        </div>
-        <div className="container-fluid bg-light mb-3">
-          <div className="row">
-            <div className="col-md-12">
-              <CardDealsOfTheDay
-                endDate={Date.now() + 1000 * 60 * 60 * 14}
-                title="Deals of the Day"
-                to="/"
-              >
-                <Carousel id="elect-product-category1">
+        <React.Fragment>
+          <Banner className="mb-3 ricki-apple-style" id="carouselHomeBanner" data={data.banner} />
+          <div className="container-fluid bg-light mb-3 ricki-apple-style">
+            <div className="row g-3">
+              <div className="col-md-9">
+                <Carousel id="elect-product-category" className="mb-3">
                   {carouselContent}
                 </Carousel>
-              </CardDealsOfTheDay>
+                <Support />
+              </div>
+              <div className="col-md-3">
+                <CardLogin className="mb-3" />
+                <CardImage src="../../images/banner/Watches.webp" to="promo" />
+              </div>
             </div>
           </div>
-        </div>
+          <div className="container-fluid bg-light mb-3 ricki-apple-style">
+            <div className="row">
+              <div className="col-md-12">
+                <CardDealsOfTheDay
+                    endDate={Date.now() + 1000 * 60 * 60 * 14}
+                    title="Deals of the Day"
+                    to="/"
+                >
+                  <Carousel id="elect-product-category1">
+                    {carouselContent}
+                  </Carousel>
+                </CardDealsOfTheDay>
+              </div>
+            </div>
+          </div>
 
-        <div className="bg-info bg-gradient p-3 text-center mb-3">
-          <h4 className="m-0">Explore Fashion Collection</h4>
-        </div>
-        <div className="container">
-          <div className="row">
-            <div className="col-md-3">
-              <Link to="/" className="text-decoration-none">
-                <img
-                  src="/images/category/men.webp"
-                  className="img-fluid rounded-circle"
-                  alt="..."
-                />
-                <div className="text-center h6">homme</div>
-              </Link>
-            </div>
-            <div className="col-md-3">
-              <Link to="/" className="text-decoration-none">
-                <img
-                  src="/images/category/women.webp"
-                  className="img-fluid rounded-circle"
-                  alt="..."
-                />
-                <div className="text-center h6">femme</div>
-              </Link>
-            </div>
-            <div className="col-md-3">
-              <Link to="/" className="text-decoration-none">
-                <img
-                  src="/images/category/accessories.webp"
-                  className="img-fluid rounded-circle"
-                  alt="..."
-                />
-                <div className="text-center h6">accessoires</div>
-              </Link>
-            </div>
-            <div className="col-md-3">
-              <Link to="/" className="text-decoration-none">
-                <img
-                  src="/images/category/footwear.webp"
-                  className="img-fluid rounded-circle"
-                  alt="..."
-                />
-                <div className="text-center h6">chaussures</div>
-              </Link>
+          <div className="bg-info bg-gradient p-3 text-center mb-3">
+            <h4 className="m-0">Timeless Elegance, Modern Trends</h4>
+          </div>
+          <div className="container">
+            <div className="row">
+              <div className="col-md-3">
+                <Link to="/" className="text-decoration-none">
+                  <img
+                      src="/images/category/men.webp"
+                      className="img-fluid rounded-circle"
+                      alt="..."
+                      style={{ width: '230', height: '230' }}  // Adjust size as needed
+
+                  />
+                  <div className="text-center h6"><br></br>homme</div>
+                </Link>
+              </div>
+              <div className="col-md-3">
+                <Link to="/" className="text-decoration-none">
+                  <img
+                      src="/images/category/women.webp"
+                      className="img-fluid rounded-circle"
+                      alt="..."
+                      style={{ width: '230', height: '230' }}  // Adjust size as needed
+
+                  />
+                  <div className="text-center h6"><br></br>femme</div>
+                </Link>
+              </div>
+              <div className="col-md-3">
+                <Link to="/" className="text-decoration-none">
+                  <img
+                      src="/images/category/accessories.webp"
+                      className="img-fluid rounded-circle"
+                      alt="..."
+                      style={{ width: '230', height: '230' }}  // Adjust size as needed
+
+                  />
+                  <div className="text-center h6"><br></br>accessoires</div>
+                </Link>
+              </div>
+              <div className="col-md-3">
+                <Link to="/" className="text-decoration-none">
+                  <img
+                      src="/images/category/footwear.webp"
+                      className="img-fluid rounded-circle"
+                      alt="..."
+                      style={{ width: '230', height: '230' }}  // Adjust size as needed
+
+                  />
+                  <div className="text-center h6"><br></br>chaussures</div>
+                </Link>
+              </div>
             </div>
           </div>
-        </div>
-      </React.Fragment>
+        </React.Fragment>
     );
   }
 }
